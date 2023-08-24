3b5f5ba7f47ae883dae7dce6cd3b8037bf7a357e
Gabriel Braga Ladislau <69354723+gbladislau@users.noreply.github.com>
2023-08-24 16:12:56 -0300

    Update quadrado.cpp

--- Lab1/quadrado.cpp
+++ Lab1/quadrado.cpp
@@ -1,152 +1,132 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <stdlib.h>
 #include <GL/glut.h>
-#include <math.h>
 #include <stdio.h>
-#define TAMANHO_JANELA 500
+#define TAMANHO_JANELA 600
 
-struct MouseCoordanates{
-   int previousX = 0.0;
-   int previousY = 0.0;
-};
-MouseCoordanates mouseCoordanates;
+float gX = 0.0;
+float gY = 0.0;
+int keyStatus[256];
 
-struct MovingPoint
-{
-   GLfloat previousX = 0.0;
-   GLfloat previousY = 0.0;
-   int pointIndex = 0;
-   bool isMoving = false;
-};
-MovingPoint movingPoint;
+float initialgX = 0.0;
+float initialgY = 0.0;
+int initialMouseX = 0;
+int initialMouseY = 0;
 
-float size = 1.0;
-
-//Pontos de controle da Spline
-GLfloat ctrlpoints[4][3] = {
-        { 0.1, 0.1, 0.0}, { 0.3, 0.9, 0.0}, 
-        {0.7, 0.1, 0.0}, {0.9, 0.9, 0.0}};
-
-
-void init(void)
-{
-   glClearColor(0.0, 0.0, 0.0, 0.0);
-   glShadeModel(GL_FLAT);
-   glEnable(GL_MAP1_VERTEX_3);
-
-   //Definicao do polinomio com os pontos de controle
-   glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]); 
-   
-   //Muda para a matriz de projecao (aulas futuras)
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-   //Define a area/volume de visualizacao. Os objetos desenhados devem estar dentro desta area
-   glOrtho(0.0, size, 0.0, size, 0.0 , size);
-}
+bool insideSquare = false;
 
 void display(void)
 {
-   int i;
+    /* Limpar todos os pixels  */
+    glClear(GL_COLOR_BUFFER_BIT);
 
-   glClear(GL_COLOR_BUFFER_BIT);
-   
-   /* Desenha a curva aproximada por n+1 pontos. */
-   int n = 30;
-   glColor3f(1.0, 1.0, 1.0);
-   glBegin(GL_LINE_STRIP);
-      for (i = 0; i <= n; i++){
-        //Avaliacao do polinomio, retorna um vertice (equivalente a um glVertex3fv) 
-        glEvalCoord1f((GLfloat) i/(GLfloat)n);
-      }
-   glEnd();
-   
-   /* Desenha os pontos de controle. */
-   glPointSize(5.0);
-   glColor3f(1.0, 1.0, 0.0);
-   glBegin(GL_POINTS);
-      for (i = 0; i < 4; i++) 
-         glVertex3fv(&ctrlpoints[i][0]);
-   glEnd();
-   glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
-   glutSwapBuffers();
+    /* Define cor dos vértices com os valores R, G e B variando de 0.0 a 1.0 */
+    glColor3f(1.0, 0.8, 0.0);
+    /* Desenhar um polígono branco (retângulo) */
+    glBegin(GL_POLYGON);
+        glVertex3f(0.25 + gX, 0.25 + gY, 0.0);
+        glVertex3f(0.75 + gX, 0.25 + gY, 0.0);
+        glVertex3f(0.75 + gX, 0.75 + gY, 0.0);
+        glVertex3f(0.25 + gX, 0.75 + gY, 0.0);
+    glEnd();
+
+    /* Desenhar no frame buffer! */
+    glutSwapBuffers(); // Funcao apropriada para janela double buffer
 }
 
-void reshape(int w, int h)
+void keyPress(unsigned char key, int x, int y)
 {
-   //Define a porcao visivel da janela
-   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
-   
-   //Muda para a matriz de projecao (aulas futuras)
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
-
-   //Controla o redimensionamento da janela mantendo o aspect ration do objeto
-   if (w <= h)
-      glOrtho(-size, size, -size*(GLfloat)h/(GLfloat)w, 
-               size*(GLfloat)h/(GLfloat)w, -size, size);
-   else
-      glOrtho(-size*(GLfloat)w/(GLfloat)h, 
-               size*(GLfloat)w/(GLfloat)h, -size, size, -size, size);
-   
-   //Muda para a matriz de trasformacoes (aulas futuras)
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+    keyStatus[(int)(key)] = 1;
+    glutPostRedisplay();
 }
 
-float recalc (int value){
-   return (float) value / TAMANHO_JANELA;
-}
-
-bool isInRange(GLfloat coordenadas[3]){
-   if (sqrt(pow(recalc(mouseCoordanates.previousX)-coordenadas[0],2)+ pow(recalc(TAMANHO_JANELA-mouseCoordanates.previousY)-(coordenadas[1]),2) <= recalc(30)))
-      return true;
-   return false;
-}
-
-void moveNearestPoint(int x, int y){
-   for (int i = 0; i < 4; i++)
-   {
-      if(isInRange(ctrlpoints[i])){
-         printf("ACHEI um  %d", i);
-         movingPoint.isMoving = true;
-         movingPoint.pointIndex = i;
-         movingPoint.previousX = ctrlpoints[i][0];
-         movingPoint.previousY = ctrlpoints[i][1];
-         break;
-      }
-   }
-}
-
-void mouse(int button, int state, int x, int y){
-   mouseCoordanates.previousX = x;
-   mouseCoordanates.previousY = y;
-   if (!state){
-      moveNearestPoint(x,y);
-   } else movingPoint.isMoving = false;
-   glutPostRedisplay();
-}
-
-void motion(int x, int y){
-   if(movingPoint.isMoving){
-      ctrlpoints[movingPoint.pointIndex][0] = (movingPoint.previousX + recalc(x - mouseCoordanates.previousX)) ;
-      ctrlpoints[movingPoint.pointIndex][1] = (movingPoint.previousY - recalc(y - mouseCoordanates.previousY)) ;
-   }
-   glutPostRedisplay();
-}
-
-int main(int argc, char** argv)
+void keyUp(unsigned char key, int x, int y)
 {
-   glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize (TAMANHO_JANELA, TAMANHO_JANELA);
-   glutInitWindowPosition (100, 100);
-   glutCreateWindow (argv[0]);
-   init ();
-   glutDisplayFunc(display);
-   glutMouseFunc(mouse);
-   glutMotionFunc(motion);
-   //glutReshapeFunc(reshape);
-   glutMainLoop();
-   return 0;
+    keyStatus[(int)(key)] = 0;
+    glutPostRedisplay();
+}
+
+void idle(void)
+{
+    if (keyStatus[(int)'a'])
+        gX += -0.0001;
+
+    if (keyStatus[(int)'s'])
+        gY += -0.0001;
+
+    if (keyStatus[(int)'w'])
+        gY += 0.0001;
+
+    if (keyStatus[(int)'d'])
+        gX += 0.0001;
+    glutPostRedisplay();
+}
+
+float rescale(int value){
+    return ((float) value/ (float)TAMANHO_JANELA);
+}
+void toggleInsideSquare(){
+    insideSquare = !insideSquare;
+}
+
+bool isInside(int x, int y){
+    return (rescale(x) >= (0.25 + gX) ) && (rescale(x) <= (gX + 0.75) ) && (rescale(TAMANHO_JANELA - y) >=  (0.25 + gY)) && (rescale(TAMANHO_JANELA - y) <= (gY + 0.75));
+}
+void mouse(int button, int state, int x, int y)
+{
+    initialMouseX = x;
+    initialMouseY = y;
+    insideSquare = false;
+    if (isInside(x,y))
+    {
+        toggleInsideSquare();
+        initialgX = gX;
+        initialgY = gY;
+    }
+
+
+    glutPostRedisplay();
+}
+
+void mouseMotion(int x, int y)
+{
+    if (insideSquare)
+    {
+        gX = initialgX + rescale(x - initialMouseX);
+        gY = initialgY - rescale(y - initialMouseY);
+
+    }
+    glutPostRedisplay();
+}
+
+void init(void)
+{
+    /* selecionar cor de fundo (preto) */
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+
+    /* inicializar sistema de visualizacao */
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(TAMANHO_JANELA, TAMANHO_JANELA);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("hello world");
+    init();
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(keyPress);
+    glutKeyboardUpFunc(keyUp);
+    glutIdleFunc(idle);
+    glutMouseFunc(mouse);
+    glutMotionFunc(mouseMotion);
+
+    glutMainLoop();
+
+    return 0;
 }
